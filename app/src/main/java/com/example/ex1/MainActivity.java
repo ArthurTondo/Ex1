99fd74766ca0da2f7f6d03f19ddfd837655856e2
Arthur Tondo <AaronVicius@gmail.com>
2020-03-15 10:25:17 -0300

    ActionBar adicionada com icone e texto!

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -12,8 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         ActionBar actionBar = getSupportActionBar();
-        setTitle("Poupanca");
-        actionBar.setIcon(R.drawable.poupanca_icon);
+        actionBar.setTitle("Poupanca");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.poupanca_icon);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
     }
 }
