fbdf652486ebba17b00141a849e8f866555e3ff0
Arthur Tondo <AaronVicius@gmail.com>
2020-03-15 10:16:09 -0300

    add action bar, e arrumei o button

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ex1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        setTitle("Poupanca");
+        actionBar.setIcon(R.drawable.poupanca_icon);
         setContentView(R.layout.activity_main);
     }
 }
